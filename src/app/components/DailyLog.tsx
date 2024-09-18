2d6cceb72483e017fd2fac8aebf4ae84cff8c120
harivigneshmasoor <hari@pipeiq.ai>
2024-09-18 08:54:52 -0700

    push

--- src/app/components/DailyLog.tsx
+++ src/app/components/DailyLog.tsx
@@ -9,7 +9,7 @@ interface Entry {
 
 const entries: Entry[] = [
   { date: '2024-09-17', weight: 176.6 },
-  { date: '2024-09-17', weight: 176.2 }
+  { date: '2024-09-18', weight: 176.2 }
   // Add more entries here as you progress
 ]
 
