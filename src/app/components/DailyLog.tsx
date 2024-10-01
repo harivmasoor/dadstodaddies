26134f2827509826bb69a423fbd1ac4c74e00418
harivigneshmasoor <hari@pipeiq.ai>
2024-10-01 07:09:32 -0700

    push

--- src/app/components/DailyLog.tsx
+++ src/app/components/DailyLog.tsx
@@ -13,7 +13,9 @@ const entries: Entry[] = [
   { date: '2024-09-19', weight: 176.6 },
   { date: '2024-09-20', weight: 176.6 },
   { date: '2024-09-25', weight: 175.2 },
-  { date: '2024-09-27', weight: 175.6 }
+  { date: '2024-09-27', weight: 175.6 },
+  { date: '2024-10-01', weight: 175.2 }
+
   // Add more entries here as you progress
 ]
 
