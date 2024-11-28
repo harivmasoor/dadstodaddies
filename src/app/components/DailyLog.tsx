6e99cde6611f470a18e564c94185bb79b1aef6bd
harivigneshmasoor <hari@pipeiq.ai>
2024-11-28 09:54:00 -0800

    new weight

--- src/app/components/DailyLog.tsx
+++ src/app/components/DailyLog.tsx
@@ -25,6 +25,7 @@ const entries: Entry[] = [
   { date: '2024-11-22', weight: 165.2 },
   { date: '2024-11-25', weight: 164.4 },
   { date: '2024-11-26', weight: 163.6 },
+  { date: '2024-11-28', weight: 163.2 },
 
   // Add more entries here as you progress
 ]
